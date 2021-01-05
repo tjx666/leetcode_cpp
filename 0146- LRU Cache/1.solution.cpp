e7f87b2d3a5011fb9a2806872b00e13af574bc9a
YuTengjing <ytj2713151713@gmail.com>
2021-01-05 23:10:48 +0800

    add git ignore and cross system debug config

--- 0146- LRU Cache/1.solution.cpp
+++ 0146- LRU Cache/1.solution.cpp
@@ -40,3 +40,18 @@ class LRUCache {
     map[key] = doubleLinkList.begin();
   }
 };
+
+int main() {
+  LRUCache lRUCache(2);
+  lRUCache.put(1, 1);  // 缓存是 {1=1}
+  lRUCache.put(2, 2);  // 缓存是 {1=1, 2=2}
+  lRUCache.get(1);     // 返回 1
+  lRUCache.put(3, 3);  // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+  lRUCache.get(2);     // 返回 -1 (未找到)
+  lRUCache.put(4, 4);  // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+  lRUCache.get(1);     // 返回 -1 (未找到)
+  lRUCache.get(3);     // 返回 3
+  lRUCache.get(4);     // 返回 4
+
+  return 0;
+}
